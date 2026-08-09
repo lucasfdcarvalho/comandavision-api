f46e9c4eec775c27f517531c3739a92d9ee12c39
Lucas Freitas de Carvalho <lucasfreitasdecarvalho15@gmail.com>
2026-08-08 22:58:52 -0300

    chore: inicializa projeto Spring Boot

---
+++ src/main/java/br/com/comandavision/api/ComandavisionApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.comandavision.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComandavisionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComandavisionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/comandavision/api/ComandavisionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.comandavision.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComandavisionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
